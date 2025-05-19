daf4ec84cad0ad1ca562811bba85de64707972f7
Tomasz Mandat <tomekmand@gmail.com>
2025-05-19 19:24:17 +0200

    Added tests

--- src/test/java/com/tomaszek/carscatalog/utils/TestLogger.java
+++ src/test/java/com/tomaszek/carscatalog/utils/TestLogger.java
@@ -8,14 +8,14 @@ public class TestLogger implements TestWatcher {
     @Override
     public void testSuccessful(ExtensionContext context) {
         String testName = context.getDisplayName();
-        System.out.println("✅ " + testName + " - PASSED");
+        System.out.println(testName + " - PASSED");
         System.out.println("──────────────────────────────────────────");
     }
 
     @Override
     public void testFailed(ExtensionContext context, Throwable cause) {
         String testName = context.getDisplayName();
-        System.out.println("❌ " + testName + " - FAILED");
+        System.out.println(testName + " - FAILED");
         System.out.println("Reason: " + cause.getMessage());
         System.out.println("──────────────────────────────────────────");
     }
